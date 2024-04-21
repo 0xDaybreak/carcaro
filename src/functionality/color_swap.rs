1a94da190ce08d550088cf7bc0a2c9a5cdae199e
Daybreak <andrei1.popescu1@gmail.com>
2024-04-21 19:19:29 +0300

    Massively changed color_swap logic and added opencv crate

--- src/functionality/color_swap.rs
+++ src/functionality/color_swap.rs
@@ -1,6 +1,8 @@
 use crate::handle_errors::Error;
-use colorsys::Hsl;
-use image::{DynamicImage, Rgba};
+use colorsys::{Hsl, Rgb};
+use image::{Rgba};
+use opencv::{imgcodecs, imgproc};
+use opencv::core::{CV_8U, Mat, MatTrait, MatTraitConst, Scalar, Vec3b};
 
 pub async fn color_swap(base_urls: Vec<String>, target_color: [u8; 3]) -> Result<(), Error> {
     println!("Started mask and models extraction");
@@ -45,18 +47,15 @@ async fn prepare_images(i: usize, url: String, dir: std::sync::Arc<String>) -> R
 pub async fn apply_color_shift(target_color: [u8; 3]) -> Result<(), Error> {
     let mut tasks = Vec::new();
     for i in 0..=11 {
-        let base_io = format!("src/base/saved_{}.png", i);
+        let base_i = format!("src/base/saved_{}.png", i);
+        let base_o = format!("src/mask/saved_{}.png", i);
 
         let task = tokio::spawn(async move {
-            let mut base_image = image::open(&base_io).expect("Failed to open image");
-            colorize_images(&mut base_image, target_color)
+            //let mut base_image = image::open(&base_io).expect("Failed to open image");
+            colorize_images(&base_i, &base_o, target_color)
                 .await
                 .expect("Failed to colorize images");
             println!("colorized images");
-            base_image
-                .to_rgba8()
-                .save(base_io)
-                .expect("Failed to save image");
         });
         tasks.push(task);
     }
@@ -69,16 +68,50 @@ pub async fn apply_color_shift(target_color: [u8; 3]) -> Result<(), Error> {
 }
 
 async fn colorize_images(
-    base_image: &mut DynamicImage,
+    base_i: &str,
+    base_o: &str,
     target_color: [u8; 3],
-) -> Result<(), Error> {
-    let mut rgba_base_image = base_image.to_rgba32f();
+) -> Result<(), opencv::Error> {
 
-    for pixel in rgba_base_image.pixels_mut() {
-        *pixel = adjust_color(&pixel, target_color);
+    if !std::path::Path::new(&base_i).exists() {
+        println!("Error: Image '{}' does not exist.", base_i);
     }
 
-    *base_image = DynamicImage::ImageRgba32F(rgba_base_image);
+
+    match extract_desired_areas(&base_i) {
+        Ok(mask) => {
+            imgcodecs::imwrite(&base_o, &mask, &opencv::core::Vector::<i32>::new()).unwrap();
+            println!("Desired areas extraction successful");
+        }
+        Err(err) => println!("Error: {}", err),
+    }
+
+    let mut original_image = match imgcodecs::imread(&base_i, imgcodecs::IMREAD_COLOR) {
+        Ok(img) => img,
+        Err(err) => {
+            println!("Error reading image '{}' {}", base_i, err);
+            return Err(err)
+        }
+    };
+
+    let mask = match imgcodecs::imread(&base_o, imgcodecs::IMREAD_GRAYSCALE) {
+        Ok(mask) => mask,
+        Err(err) => {
+            println!("Error reading mask {}", err);
+            return Err(err)
+
+        }
+    };
+    let rgb = Rgb::from(target_color);
+    let hsv_target = Hsl::from(&rgb);
+
+    let hsv_target_hue = hsv_target.hue() ;
+    let hsv_target_saturation = hsv_target.saturation();
+    let hsv_target_value = hsv_target.lightness();
+
+    apply_color_change(&mut original_image, &mask, hsv_target_hue, hsv_target_saturation, hsv_target_value);
+
+    imgcodecs::imwrite(&base_i, &original_image, &opencv::core::Vector::<i32>::new()).unwrap();
     Ok(())
 }
 
@@ -91,15 +124,21 @@ fn adjust_color(base_pixel: &Rgba<f32>, target_color: [u8; 3]) -> Rgba<f32> {
     let target_g = target_color[1] as f64;
     let target_b = target_color[2] as f64;
 
-    let target_rgb = colorsys::Rgb::new(target_r, target_g, target_b, None);
+    let target_rgb = Rgb::new(target_r, target_g, target_b, None);
     let target_hue = Hsl::from(target_rgb).hue();
+    let target_rgb = Rgb::new(target_r, target_g, target_b, None);
+    let target_saturation = Hsl::from(target_rgb).saturation();
 
-    let base_rgb: colorsys::Rgb = colorsys::Rgb::new(r, g, b, None);
+    let base_rgb: Rgb = Rgb::new(r, g, b, None);
     let mut base_hsl = Hsl::from(base_rgb);
+    if base_hsl.lightness() > 15. && base_hsl.lightness() < 75. {
         base_hsl.set_hue(0.);
         base_hsl.set_hue(target_hue);
+        base_hsl.set_saturation(0.);
+        base_hsl.set_saturation(target_saturation);
+    }
 
-    let base_rgb: colorsys::Rgb = colorsys::Rgb::from(&mut base_hsl);
+    let base_rgb: Rgb = colorsys::Rgb::from(&mut base_hsl);
 
     Rgba([
         base_rgb.red() as f32,
@@ -108,3 +147,69 @@ fn adjust_color(base_pixel: &Rgba<f32>, target_color: [u8; 3]) -> Rgba<f32> {
         base_pixel[3],
     ])
 }
+
+
+fn extract_desired_areas(image_path: &str) -> Result<Mat, opencv::Error> {
+    let image = imgcodecs::imread(image_path, imgcodecs::IMREAD_COLOR)?;
+
+    let mut hsv_image = Mat::default();
+    imgproc::cvt_color(&image, &mut hsv_image, imgproc::COLOR_BGR2HSV, 0)?;
+
+    let lower_bound = Scalar::new(10.0, 1.0, 10.0, 0.0); // Lower bound for hue, saturation, and value
+    let upper_bound = Scalar::new(45.0, 255.0, 255.0, 255.0); // Upper bound for hue, saturation, and valu
+
+    let mut desired_mask = Mat::default();
+    opencv::core::in_range(&hsv_image, &lower_bound, &upper_bound, &mut desired_mask)?;
+
+    let mut desired_mask_output = Mat::default();
+
+    let kernel = Mat::ones(1, 1, CV_8U)?;
+    imgproc::morphology_ex(
+        &desired_mask,
+        &mut desired_mask_output,
+        imgproc::MORPH_OPEN,
+        &kernel,
+        opencv::core::Point::new(-1, -1),
+        3,
+        opencv::core::BORDER_ISOLATED,
+        opencv::core::Scalar::default(),
+    )?;
+
+
+    Ok(desired_mask_output)
+}
+
+fn apply_color_change(original_image: &mut Mat, mask: &Mat, target_hue: f64, target_saturation: f64, target_value: f64) {
+    for y in 0..original_image.rows() {
+        for x in 0..original_image.cols() {
+            let mask_value = mask.at_2d::<u8>(y, x).unwrap();
+
+            if *mask_value == 255 {
+                let bgr_pixel = original_image.at_2d_mut::<Vec3b>(y, x).unwrap();
+
+                let rgba = Rgb::new(bgr_pixel[2] as f64, bgr_pixel[1] as f64, bgr_pixel[0] as f64, None);
+                let mut hsla: Hsl = rgba.as_ref().into();
+
+                let original_lightness = hsla.lightness();
+                let delta_lightness = target_value - original_lightness;
+                let transformed_lightness = original_lightness + non_linear_transform(delta_lightness);
+
+                hsla.set_hue(target_hue);
+                hsla.set_saturation(target_saturation);
+                hsla.set_lightness(transformed_lightness);
+
+
+                let rgb_arr: [u8; 3] = Rgb::from(&hsla).into();
+
+                bgr_pixel[0] = rgb_arr[2];
+                bgr_pixel[1] = rgb_arr[1];
+                bgr_pixel[2] = rgb_arr[0];
+            }
+        }
+    }
+}
+
+fn non_linear_transform(delta_lightness: f64) -> f64 {
+    delta_lightness.signum() * delta_lightness.abs().powf(0.87)
+}
+
